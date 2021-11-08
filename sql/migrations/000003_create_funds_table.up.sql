c8fbe30e11df45c4b96bc81ab82c6b0e2477d04b
Mykola Zahorodniuk <mykola.zahorodniuk@yalantis.net>
2021-11-08 10:46:53 +0200

    fix/tabs replaced with 4 spaces

--- sql/migrations/000003_create_funds_table.up.sql
+++ sql/migrations/000003_create_funds_table.up.sql
@@ -3,7 +3,7 @@ BEGIN;
 CREATE TABLE funds (
     id          UUID      NOT NULL,
     name        VARCHAR   NOT NULL,
-	ticker      VARCHAR   NOT NULL,
+    ticker      VARCHAR   NOT NULL,
     description TEXT      NOT NULL,
     created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at  TIMESTAMP NOT NULL DEFAULT NOW()
--- sql/migrations/000004_create_holdings_table.up.sql.sql
+++ sql/migrations/000004_create_holdings_table.up.sql.sql
@@ -3,7 +3,7 @@ BEGIN;
 CREATE TABLE holdings (
     fund_ticker VARCHAR    NOT NULL,
     name        VARCHAR    NOT NULL,
-	share       VARCHAR    NOT NULL,
+    share       VARCHAR    NOT NULL,
     weight      VARCHAR    NOT NULL,
     created_at  TIMESTAMP  NOT NULL DEFAULT NOW(),
     updated_at  TIMESTAMP  NOT NULL DEFAULT NOW()
